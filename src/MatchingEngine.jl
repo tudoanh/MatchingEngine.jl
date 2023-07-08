bc599b315f2974407b538192e3fc993b44049653
tudoanh <tu0703@gmail.com>
2023-07-08 15:01:33 +0700

    first commit

---
+++ src/MatchingEngine.jl
@@ -0,0 +1,26 @@
+module MatchingEngine
+using AVLTrees
+using Base: @kwdef
+using Printf
+include("orderqueue.jl")
+include("sidebook.jl")
+include("book.jl")
+include("ordermatching.jl")
+export BUY_ORDER, SELL_ORDER, VANILLA_FILLTYPE, IMMEDIATEORCANCEL_FILLTYPE, FILLORKILL_ORDER
+export OrderBook, Order, OrderTraits, AcctMap, OrderSide
+export submit_order!,
+    submit_limit_order!,
+    cancel_order!,
+    submit_market_order!,
+    submit_market_order_byfunds!,
+    clear_book!,
+    book_depth_info,
+    volume_bid_ask,
+    best_bid_ask,
+    n_orders_bid_ask,
+    bid_orders,
+    ask_orders,
+    get_acct,
+    write_csv,
+    order_types
+end
---
+++ test/runtests.jl
@@ -0,0 +1,7 @@
+using MatchingEngine
+using Test
+using Base.Iterators: zip,cycle,take,filter
+
+@testset "MatchingEngine.jl" begin
+    include("./test-1.jl")
+end
